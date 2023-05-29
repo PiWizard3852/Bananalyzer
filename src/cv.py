1bde7d4ee226a3932323760a8059346d0066e1db
Owen Wood <owenwood0101@icloud.com>
2023-05-29 10:11:18 -0400

    Improve Tile Mask

--- src/cv.py
+++ src/cv.py
@@ -6,21 +6,19 @@ image = cv2.imread(filepath)
 
 hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-lower_tan = np.array([20,  0, 0])
-upper_tan = np.array([25, 255, 255])
+lowerLimit = np.array([17, 12, 0])
+upperLimit = np.array([29, 255, 255])
 
-mask = cv2.inRange(hsv, lower_tan, upper_tan)
+tileMask = cv2.inRange(hsv, lowerLimit, upperLimit)
+tiles = cv2.bitwise_and(image, image, mask=tileMask)
 
-result = cv2.bitwise_and(image, image, mask = mask)
-edges = cv2.Canny(result,100,200)
+edgeMask = cv2.Canny(tiles, 100, 200)
+edges = cv2.bitwise_and(image, image, mask=edgeMask)
 
-result2 = cv2.bitwise_and(image, image, mask = edges)
-
-contours, hierarchy = cv2.findContours(edges, 
-    cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+contours, hierarchy = cv2.findContours(edgeMask,
+                                       cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
 cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
-
 
 # cnts = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 # cnts = cnts[0] if len(result2) == 2 else cnts[1]
@@ -29,6 +27,5 @@ cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
 #     cv2.drawContours(image,[c], 0, (0,255,0), 3)
 
 
-
-cv2.imshow("Im", image)
+cv2.imshow("Image", image)
 cv2.waitKey(0)
