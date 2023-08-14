c0514b56d1dd33fffd7ed4fce9007bf3575ded60
Owen Wood <owenwood0101@icloud.com>
2023-08-14 10:24:42 -0400

    feat: insert word

--- src/grid.py
+++ src/grid.py
@@ -1,3 +1,7 @@
+from functools import lru_cache
+
+
+@lru_cache
 def getGridDimensions():
     longest = ""
 
@@ -9,6 +13,18 @@ def getGridDimensions():
     return len(longest)
 
 
-def initGrid():
-    gridLength = getGridDimensions()
-    return [[0]*gridLength]*gridLength
+class Grid:
+    def __init__(self):
+        length = getGridDimensions()
+
+        self.grid = [["" for i in range(length)] for n in range(length)]
+        self.words = []
+
+    def insertWord(self, word, x, y, horizontal):
+        self.words.append([word, x, y, horizontal])
+
+        for i in range(len(word)):
+            if horizontal:
+                self.grid[y][x + i] = word[i]
+            else:
+                self.grid[y + i][x] = word[i]
